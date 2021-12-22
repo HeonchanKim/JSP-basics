642db2b84e6bf2dfd06dc00827f9b1bc68832eab
Heonchankim <khc9274@naver.com>
2021-12-22 22:09:00 +0900

    day03

---
+++ workspace/day05/src/main/java/com/online/servlet/Clac.java
@@ -0,0 +1,19 @@
+package com.online.servlet;
+
+public class Clac {
+	int num1;
+	int num2;
+	
+	public Clac() {}
+	
+	public Clac(int num1, int num2) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	// 덧셈
+	// 뺄셈
+	// 곱셈
+	// 나눗셈
+}
