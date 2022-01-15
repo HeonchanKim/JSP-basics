14a3d69a7d5e020f086fe71a59e82950a4be4cef
HeonchanKim <khc9274@naver.com>
2022-01-15 22:47:59 +0900

    day24

--- workspace/board_mvc/src/com/khc/app/member/controller/MemberFrontController.java
+++ workspace/board_mvc/src/com/khc/app/member/controller/MemberFrontController.java
@@ -2,10 +2,13 @@ package com.khc.app.member.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.khc.app.action.ActionForward;
 
 public class MemberFrontController extends HttpServlet{
 	
@@ -18,6 +21,31 @@ public class MemberFrontController extends HttpServlet{
 		doProcess(req, resp);	
 	}
 	protected void doProcess(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException {
+		String requestURI = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		ActionForward forward = null;
 		
-	}
+		if(command.equals("/member/MemberCheckIdOk.me")) {
+			try {
+				forward = new MemberCheckIdOk().execute(req, resp);
+			} catch (Exception e) {
+				System.out.println("아이디 중복검사 오류 " + e);
+			}
+		}
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				resp.sendRedirect(forward.getPath());
+			}else {
+				RequestDispatcher dispatcher = req.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(req, resp);
+			}
+		}
+		
+		
+		
+		
+		
+	}//end of doProcess
 }
